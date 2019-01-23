6107370fba978c0e5b6a733da9a65107833fd3c1
Jinliang Yang <yangjl0930@gmail.com>
2019-01-22 20:05:24 -0600

    run landrace imputation

---
+++ profiling/3.popgen/3.A.4_setup_ajob_land.R
@@ -0,0 +1,25 @@
+### Jinliang Yang
+### 01-22-2019
+### set up array job
+
+##get command line args
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+
+JOBID <- as.numeric(as.character(args[1]))
+print(JOBID)
+
+source("lib/doimpute.R")
+#source("lib/mcmcbc.R")
+library("data.table")
+library("plyr")
+
+files <- list.files(path="largedata/mr_100bp", pattern="landrace_", full.names=TRUE)
+
+mx <- fread(files[JOBID], data.table=FALSE)
+out <- doimpute(mx, ncols=2:17, binsize=1000)
+
+outfile <- gsub("matrix", "matrix_imp", files[JOBID])
+fwrite(out, outfile, sep=",", row.names=FALSE, quote=FALSE)
+#####
+
