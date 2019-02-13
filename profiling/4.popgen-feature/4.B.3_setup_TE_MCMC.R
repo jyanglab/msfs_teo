e20f87a2e304ded8eb5f8b85a2f5b612fd3e1373
Jinliang Yang <yangjl0930@gmail.com>
2019-02-13 17:06:35 -0600

    running TE MCMC ...

---
+++ profiling/4.popgen-feature/4.B.3_setup_TE_MCMC.R
@@ -0,0 +1,29 @@
+### Jinliang Yang
+### 10-14-2016
+### run MCMC for comet on chr10
+
+##get command line args
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+
+JOBID <- as.numeric(as.character(args[1]))
+print(JOBID)
+
+source("lib/mplots.R")
+source("lib/mcmcbc.R")
+
+files <- list.files(path="cache", pattern="TE_sfs_", full.names=TRUE)
+sfs <- read.csv(files[JOBID])
+
+set.seed(12345679)
+# If acceptance too high, increase these values to explore wider space. If acceptance too low, decrease.
+res <- MCMCBC(my_sfs=sfs$Freq, rates=c(1E8,1E8,1E5), sd=c(0.05,0.05,0.05), k=0:102,
+              conditional=FALSE, Ne=150000, ngen=1000000, verbose=TRUE)
+
+#####
+#out <- gsub("cache", "largedata", files[JOBID])
+out <- gsub("csv", "RData", files[JOBID])
+save(list="res", file=out)
+### plot trace and posteriors
+
+
