8ed0064b648fd3430d42b88e67ae82f6d1724db7
Jinliang Yang <yangjl0930@gmail.com>
2020-07-04 11:51:10 -0500

    updates

---
+++ profiling/4.popgen-feature/4.A.1.2_setup_feature_MCMC.R
@@ -0,0 +1,29 @@
+### Jinliang Yang
+### 07-04-2020
+### run MCMC for comet on chr10
+
+##get command line args
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+
+JOBID <- as.numeric(as.character(args[1]))
+print(JOBID)
+
+source("lib/mplots.R")
+source("lib/mcmcbc.R")
+
+
+#files <- list.files(path="cache/mcmc_res", pattern="new_msfs", full.names=TRUE)
+df <- read.csv("slurm-script/slurm_feature_parameters.csv")
+sfs <- read.csv(as.character(df$file[JOBID]))
+
+set.seed(12345)
+# If acceptance too high, increase these values to explore wider space. If acceptance too low, decrease.
+res <- MCMCBC(my_sfs=sfs$Freq, rates=c(1E6,1E6,1E6), sd=c(0.05,0.05,0.05), k=0:(nrow(sfs)-1),
+              conditional=FALSE, Ne=df$ne[JOBID], ngen=100000, verbose=TRUE)
+
+#####
+#out <- gsub("cache", "largedata", files[JOBID])
+#out <- gsub("csv", "RData", df$file[JOBID])
+save(list="res", file=as.character(df$out[JOBID]))
+### plot trace and posteriors
