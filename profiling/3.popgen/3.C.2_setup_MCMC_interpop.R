856f50a36a1586a284b1719eb47fdfb2c9a1e2ef
Jinliang Yang <yangjl0930@gmail.com>
2020-07-06 22:27:03 -0500

    fig1c

--- profiling/3.popgen/3.C.2_setup_MCMC_interpop.R
+++ profiling/3.popgen/3.C.2_setup_MCMC_interpop.R
@@ -1,5 +1,5 @@
 ### Jinliang Yang
-### 10-14-2016
+### 10-14-2016 updated: 7-6-2020
 ### run MCMC for comet on chr10
 
 ##get command line args
@@ -12,18 +12,19 @@ print(JOBID)
 source("lib/mplots.R")
 source("lib/mcmcbc.R")
 
-files <- list.files(path="cache/mcmc_res", pattern="new_msfs", full.names=TRUE)
-sfs <- read.csv(files[JOBID])
 
-set.seed(12345679)
+#files <- list.files(path="cache/mcmc_res", pattern="new_msfs", full.names=TRUE)
+df <- read.csv("slurm-script/slurm_parameters_fig1.csv")
+sfs <- read.csv(as.character(df$file[JOBID]))
+
+set.seed(12345)
 # If acceptance too high, increase these values to explore wider space. If acceptance too low, decrease.
-res <- MCMCBC(my_sfs=sfs$Freq, rates=c(1E8,1E8,1E5), sd=c(0.05,0.05,0.05), k=0:(nrow(sfs)-1),
-              conditional=FALSE, Ne=150000, ngen=1000000, verbose=TRUE)
+res <- MCMCBC(my_sfs=sfs$Freq, rates=c(1E6,1E6,1E6), sd=c(0.05,0.05,0.05), k=0:(nrow(sfs)-1),
+              conditional=FALSE, Ne=df$ne[JOBID], ngen=1000000, verbose=TRUE)
 
 #####
 #out <- gsub("cache", "largedata", files[JOBID])
-out <- gsub("csv", "RData", files[JOBID])
-save(list="res", file=out)
+#out <- gsub("csv", "RData", df$file[JOBID])
+save(list="res", file=as.character(df$out[JOBID]))
 ### plot trace and posteriors
-
 
